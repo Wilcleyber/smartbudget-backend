20613265c9b609df6123b507ff239c1ba6c444da
Wilcleyber <wilcleyber2@gmail.com>
2025-12-16 05:52:34 -0300

    corrigir ordem de criação do app e inclusão de rotas

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.routes import router
 from api import analytics_routes
-app.include_router(analytics_routes.router)
 
 app = FastAPI()
 
+# Configuração de CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-
     allow_credentials=True,
     allow_methods=["*"],
-
     allow_headers=["*"],
 )
 
+# Rotas principais
 app.include_router(router)
+
+# Rotas de análise
+app.include_router(analytics_routes.router)
